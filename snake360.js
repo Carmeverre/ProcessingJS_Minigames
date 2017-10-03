7885c146da5cb1af033a75e03035841a5956b374
Carmeverre <Carmeverre@gmail.com>
2017-10-03 20:43:00 +0300

    snake movement
    Not working correctly yet. Also updated event handling.

--- snake360.js
+++ snake360.js
@@ -9,8 +9,8 @@ angleMode = "degrees";
 var START_SEGMENTS = 3; // increase to skip to higher difficulty. Must be at least 1.
 var SEGMENT_SIZE = 20;
 var MOVE_SPEED = 5;
+var ROTATION_SPEED = 1; // rotation speed (control response)
 // todo: snake color
-// todo: rotation speed (control response)
 
 
 
@@ -26,8 +26,18 @@ var Segment = function(x,y,dir) {
 	// center:
 	this.x = x;
 	this.y = y;
+	
+	// movement:
+	this.dir = dir; // direction in degrees from right side
+	
+	// drawing:
 	this.r = SEGMENT_SIZE/2;
-	this.dir = dir; // direction in degrees from side
+};
+
+Segment.prototype.move = function() {
+    this.x += MOVE_SPEED * cos(this.dir);
+    this.y += MOVE_SPEED * sin(this.dir);
+	ellipse(this.x, this.y, SEGMENT_SIZE, SEGMENT_SIZE);
 };
 
 Segment.prototype.draw = function() {
@@ -46,7 +56,7 @@ var Snake = function() {
 	var tailStartX = width/2;
 	var tailStartY = height - 2*SEGMENT_SIZE;
 	for(var i = 0; i < START_SEGMENTS; i++) {
-		var seg = new Segment(tailStartX, tailStartY - (START_SEGMENTS)*SEGMENT_SIZE);
+		var seg = new Segment(tailStartX, tailStartY + (START_SEGMENTS*SEGMENT_SIZE), 270);
 		this.segments.push(seg);
 	}
 	
@@ -56,8 +66,22 @@ var Snake = function() {
 	this.score = 0;
 };
 
+Snake.prototype.turn = function(angleDelta) {
+    // angleDelta in degrees clockwise
+    this.segments[0].dir = (this.segments[0].dir+angleDelta) % 360;
+};
+
+Snake.prototype.move = function() {
+    for(var i = this.segments.length-1; i >= 1; i--) {
+		this.segments[i].dir = this.segments[i-1].dir;
+	}
+	for(var i = 0; i < this.segments.length; i++) {
+		this.segments[i].move();
+	}
+};
+
 Snake.prototype.draw = function() {
-	for(var i = 1; i < this.segments.length; i++) {
+	for(var i = 0; i < this.segments.length; i++) {
 		this.segments[i].draw();
 	}
 	// TODO: draw eyes on the head (first) segment to indicate which direction the snake is going
@@ -82,18 +106,20 @@ var playerSnake = new Snake();
 /** EVENT HANDLERS **/
 /********************/
 
-
 draw = function() {
+    background(255, 255, 255);
 	// +=MOVE_SPEED; // delta, should be broken into x and y, or something should be done wih rotate
 	// would rotate work for each segment separately? Will it just save me explicit trig calculations?
 	// meh, implement with trig, easier to debug...
 	if(keyIsPressed) {
 		if(keyPressed === RIGHT) {
 			// change direction of front segment
+			playerSnake.turn(-ROTATION_SPEED);
 		}
 		else if(keyPressed === LEFT) {
-			// opposite of above // todo: take it out into a function that gets possibly negative delta or angle
+			playerSnake.turn(ROTATION_SPEED);
 		}
 	}
+	playerSnake.move();
 	playerSnake.draw();
 };
